387755642b20d3e6a8c2d728b89efd4307f818a6
RobertEnjematech <nasoarobert82@gmail.com>
2025-06-12 18:09:08 +0100

    update

--- signlink/app/_layout.tsx
+++ signlink/app/_layout.tsx
@@ -1,10 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import 'react-native-reanimated';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -24,6 +29,7 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
+
     </ThemeProvider>
   );
 }
