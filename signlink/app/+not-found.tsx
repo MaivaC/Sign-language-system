51c59bbc9f850de96645c298961584a6b1f185a6
RobertEnjematech <nasoarobert82@gmail.com>
2025-06-12 01:05:14 +0100

    updated_with_boiler+plate

--- signlink/app/+not-found.tsx
+++ signlink/app/+not-found.tsx
@@ -1,4 +1,7 @@
-import { Link, Stack } from 'expo-router';
+import {
+  Link,
+  Stack,
+} from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
