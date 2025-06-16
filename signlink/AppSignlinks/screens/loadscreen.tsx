7f3ad537cf14857481697633b74de4d5af90479f
RobertEnjematech <nasoarobert82@gmail.com>
2025-06-16 03:27:54 +0100

    loadingscreen.tsx

--- signlink/AppSignlinks/screens/loadscreen.tsx
+++ signlink/AppSignlinks/screens/loadscreen.tsx
@@ -20,10 +20,19 @@ const Loadingscreen: React.FC = () => {
   return (
     <View style={ dashboardstyles .container}>
 
+          <View style={{ margin:"6%", backgroundColor: "#E35817", height: "90%", width: "90%",borderRadius:20,justifyContent:"flex-start",alignItems:"center"}}>
+              <Text  style={{color:"#fff", fontSize:15,fontWeight:"bold"}}>WELCOME TO </Text>
+              <Text  style={{color:"#fff", fontSize:30,fontWeight:"bold",marginBottom:"24%"}}>SIGNLINK</Text>
 
-  
-          <View style={{ margin:"6%", backgroundColor: "#E35817", height: "90%", width: "90%"}}>
-              <Text>Hi</Text>
+
+<View style={{ height: "55%", width: "90%", borderRadius:20, backgroundColor:"#fff"}}>
+
+                  
+</View>
+
+<Text  style={{color:"#fff", fontSize:15,fontWeight:"bold",marginTop:"22%"}}>WELCOME TO </Text>
+              <Text  style={{color:"#fff", fontSize:30,fontWeight:"bold"}}>SIGNLINK</Text>
+
               
       </View>       
 
--- signlink/AppSignlinks/styles/generalappstyles.tsx
+++ signlink/AppSignlinks/styles/generalappstyles.tsx
@@ -132,108 +132,18 @@ alignContent: "center",
     width: '100%',
     height: "100%",
     backgroundColor: colors.background,
-    opacity: 1
+     opacity: 1,
+     justifyContent: "center",
+    alignContent:"center"
   },
   description: {
     flex:3,
     color:colors.primary
   },
 
-  // Top Box (40% of screen)
-  box1: {
-    flex: 1.5,
-    backgroundColor: colors.primary,
-    flexDirection:"row",
-    justifyContent: 'space-evenly',
 
 
-    
-  },
-  menuclass: {
-    position: 'absolute',
-    left: 0,              // Aligns the element to the left edge
-    top: '50%',           // Optional: center vertically
-    transform: [{ translateY: -25 }], // Optional: fine-tune vertical centering
-    zIndex: 10,           // Ensures it stays on top
-    padding: 10,          // Optional: spacing
-  },
-  Text1: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#fff',
-    paddingHorizontal: 16,
-  },
-
-  // Middle Box (50% of screen)
-  box2: {
-    flex: 14,
-    alignItems: 'center',
-  },
-  Text2: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: colors.background,
-    paddingHorizontal: 16,
- 
-  },
-
-  // Subbox inside box2
 
 
-  // Bottom Matrix Box (10% of screen)
-  matrixbox: {
-    flex: 1,
-    backgroundColor: colors.background,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height:315.5
-  },
-
-  bottomBar: {
-    position: 'absolute',     // <-- key change
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 60,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-
-
-    paddingHorizontal: 10,
-    zIndex: 10,               // ensures it's on top
-  },
-  
-  dotsmall: {
-    width: 35,
-    height: 35,
-    borderRadius: 17,
-    backgroundColor: colors.background,
-    alignItems: "center",
-    justifyContent: "center",
-    alignContent:"center"
-    
-  },
-  dotlarge: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: colors.background,
-
-  },
-  iconButton: {
-    backgroundColor: '#F5DEB3', // Light brown / wheat
-    padding: 12,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    elevation: 20, // Android shadow
-    shadowColor: '#000', // iOS shadow
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-  }
 });
 
